b7661d9b9e63b4dd5e0f776bb61de0796928bdaa
thepjos <josh.thephasdin@gmail.com>
2023-03-03 13:18:06 -0600

    add feeding and details for feeding

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # import your models here
-from .models import Cat
+from .models import Cat, Feeding
 
 # Register your models here
 admin.site.register(Cat)
 
-
+# register the new Feeding Model 
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,15 @@ from django.urls import reverse
 
 # Create your models here.
 
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+# new code above
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -15,3 +24,16 @@ class Cat(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cat_id': self.id })
+    
+# Add new Feeding model below Cat model
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices = MEALS,
+        default = MEALS[0][0]
+    )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    def __str__(self):
+    
+        return f"{self.get_meal_display()} on {self.date}"
