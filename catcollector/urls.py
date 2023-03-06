f69b49cac69100edadfca08054c883e928645a74
thepjos <josh.thephasdin@gmail.com>
2023-03-06 13:01:11 -0600

    authentication fixes, building views and log in features

--- catcollector/urls.py
+++ catcollector/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # '' represents the "starts with" path
     path('', include('main_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -26,6 +27,7 @@ class Cat(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def fed_for_today(self):
         return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
